d563f1789349b486d979b2e98586998e230fc247
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-03-11 09:20:43 +1300

    first commit

---
+++ supreme_db.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+DATABASE_FILE = "Pizza_database.db"
+
+
+#cursor = connection.cursor()
+#sql = f"SELECT * FROM {table}"
+#cursor.execute(sql) #executes the SELECT statement on the chosen table
+#results = cursor.fetchall() #stores the results in the results variable
+
+
+#with sqlite3.connect(DATABASE_FILE) as connection:
+#    pass
+
+print("test")
+#file_test = open("text.txt", "r")
+
+#print(file_test.read())
+print("test")
+
