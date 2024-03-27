ad2dd2b7c5c9e8f934fc4888b22eae36a787d09c
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-03-27 14:30:08 +1300

    got around python scoping using class and not all data extraction works

--- supreme_db.py
+++ supreme_db.py
@@ -59,40 +59,39 @@ def get_number_from_line(line):
 def iterate_through_file():
     ''' loops through unit blueprint and extracts and sets all required variables out'''
     line_number = 0
-    for line in unit_blueprint:
+    for line in unit.blueprint:
         if line_number == 1:
-            unit_name = get_name_from_line(line)
+            unit.name = get_name_from_line(line)
         
-        if "= {" in line and "    " in line and "     " not in line: #detects for beginnings of a second level dictionary
-            print("test")
-            current_section = get_category_from_line(line)
+        if "= {" in line and "    " in line and "     " not in line:  # detects for beginnings of a second level dictionary
+            unit.current_section = get_category_from_line(line)
         
-        if current_section == "Categories":
+        if unit.current_section == "Categories":
             if '"AIR"' in line:
-                roles["Air"] = True
+                unit.roles["Air"] = True
             if '"NAVAL"' in line:
-                roles["Naval"] = True
+                unit.roles["Naval"] = True
             if '"LAND"' in line:
-                roles["Land"] = True
+                unit.roles["Land"] = True
             if '"AMPHIBIOUS"' in line:
-                roles["Land"] = True
-                roles["Naval"] = True
+                unit.roles["Land"] = True
+                unit.roles["Naval"] = True
             if '"ANTIAIR"' in line:
-                roles["Anti Air"] = True
+                unit.roles["Anti Air"] = True
             if '"ANTINAVY"' in line:
-                roles["Anti Naval"] = True
+                unit.roles["Anti Naval"] = True
             
-        if current_section == "Defense":
+        if unit.current_section == "Defense":
             if ' Health = ' in line:
-                unit_health = get_number_from_line(line)
+                unit.health = get_number_from_line(line)
         
-        if current_section == "Economy":
+        if unit.current_section == "Economy":
             if ' BuildCostEnergy =' in line:
-                unit_energy_cost = get_number_from_line(line)
+                unit.energy_cost = get_number_from_line(line)
             if ' BuildCostMass =' in line:
-                unit_mass_cost = get_number_from_line(line)
+                unit.mass_cost = get_number_from_line(line)
             if ' BuildTime =' in line:
-                unit_build_time = get_number_from_line(line)
+                unit.build_time = get_number_from_line(line)
         line_number += 1
 
 
@@ -103,11 +102,10 @@ def add_unit_to_supreme_database():
         cursor.execute(sql) #executes the SELECT statement on the chosen table
 
 
-
 def process_file(filename):
-    #UAA0101 [0] is unit  [1] is faction   [2] is unit type  [4] is tech level
+    # UAA0101 [0] is unit  [1] is faction   [2] is unit type  [4] is tech level
 
-    #filters out non units and civilians and buildings
+    # filters out non units and civilians and buildings
     if len(filename) != 7:
         return
     if filename[0] != "U" and filename[0] != "X":
@@ -117,22 +115,22 @@ def process_file(filename):
     if filename[1] == "X":
         return
 
-    #initialise variables
-    unit_blueprint = ""
+    # initialise variables
+    unit.blueprint = ""
     try:
-        text_file = open(f"{units_path}{filename}/{filename}_unit.bp",'r')
-        unit_blueprint = text_file.readlines()
-    except: # unacceptable file pattern detected
+        text_file = open(f"{units_path}{filename}/{filename}_unit.bp", 'r')
+        unit.blueprint = text_file.readlines()
+    except:  # unacceptable file pattern detected
         return
-    current_section = ""
-    unit_name = ""
-    unit_health = 0
-    unit_tech_level = int(filename[4])
-    unit_faction = faction_conversion[filename[1]]
-    unit_mass_cost = 0
-    unit_energy_cost = 0
-    unit_build_time = 0
-    roles = {
+    unit.current_section = ""
+    unit.name = ""
+    unit.health = 0
+    unit.tech_level = int(filename[4])
+    unit.faction = faction_conversion[filename[1]]
+    unit.mass_cost = 0
+    unit.energy_cost = 0
+    unit.build_time = 0
+    unit.roles = {
         "Air": False,
         "Naval": False,
         "Land": False,
@@ -140,14 +138,10 @@ def process_file(filename):
         "Anti Naval": False
     }
 
-    #find tech 3 mobile anti aa seraphim
-
-    
 
     iterate_through_file()
-    if unit_tech_level == 3 and roles["Anti Air"] and roles["Land"] and unit_faction == "Seraphim":
-        print(unit_name)
-        print("test")
+    if unit.roles["Anti Air"] and unit.roles["Land"]:
+        print(unit.name, unit.health, unit.faction, unit.mass_cost, unit.energy_cost, unit.build_time)
     return
 
     add_unit_to_supreme_database()
@@ -156,23 +150,22 @@ def process_file(filename):
 
 
 #initialise variables
-unit_blueprint = ""
-current_section = ""
-unit_name = ""
-unit_health = 0
-unit_tech_level = 0
-unit_faction = "nothing"
-unit_mass_cost = 0
-unit_energy_cost = 0
-unit_build_time = 0
-roles = {
-    "Air": False,
-    "Naval": False,
-    "Land": False,
-    "Anti Air": False,
-    "Anti Naval": False
-}
+class unit:
+    blueprint = ""
+    current_section = ""
+    name = ""
+    health = 0
+    tech_level = 0
+    faction = "nothing"
+    mass_cost = 0
+    energy_cost = 0
+    build_time = 0
+    roles = {
+        "Air": False,
+        "Naval": False,
+        "Land": False,
+        "Anti Air": False,
+        "Anti Naval": False
+    }
 for filename in os.listdir(units_path):
     process_file(filename)
-
-
