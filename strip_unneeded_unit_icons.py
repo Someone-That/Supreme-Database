810958ae3a8dd0be50dd3b29635a17873c79bfd9
Mikaeel <mikaeel.aykhan@gmail.com>
2024-07-17 17:47:00 +1200

    stripped irrelevant icons, cleaned names, converted dds to png

--- strip_unneeded_unit_icons.py
+++ strip_unneeded_unit_icons.py
@@ -21,15 +21,34 @@ all_icons = os.listdir(icons_path)
 code_extraction = sql_statement("SELECT code FROM Units;")
 
 delete_list = []
+outlier_list = []
 
+#  rename all icons to just the code and capitalise
 for icon in all_icons:
-    should_delete = True
-    for code in code_extraction:
-        if code[0] in icon:
-            should_delete = False
-            break
-    if should_delete:
-        delete_list.append(icon)
-        os.remove(f"{icons_path}{icon}")
+    os.rename(f"{icons_path}{icon}", f"{icons_path}{icon[:7].upper()}.png")
+
+#  check for units not included
+# for code in code_extraction:
+#     should_delete = True
+#     for icon in all_icons:
+#         uc = code[0]
+#         uc = uc.lower()
+#         if uc in icon.lower():
+#             should_delete = False
+#             break
+#     if should_delete:
+#         outlier_list.append(code)
+# print(outlier_list)
+
+#  delete all irrelevant icons
+# for icon in all_icons:
+#     should_delete = True
+#     for code in code_extraction:
+#         if code[0].lower() in icon.lower():
+#             should_delete = False
+#             break
+#     if should_delete:
+#         delete_list.append(icon)
+#         os.remove(f"{icons_path}{icon}")
 
 
